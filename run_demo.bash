c64a1a03f5f9a1f82e6a994e5e68b65c20aa22e3
korenleven <korenleven@gmail.com>
2022-02-24 14:41:17 +0200

    Update run_demo.bash

--- run_demo.bash
+++ run_demo.bash
@@ -1,13 +1,13 @@
 #!/bin/sh
 LANG=en_US
 export CUDA_VISIBLE_DEVICES=1
-cp -r /mnt/smplify_x /bodyshop/smplify_x
-cp /mnt/openpose_python.py /bodyshop/openpose_python.py
-cp /mnt/conversions.py /usr/local/lib/python3.8/dist-packages/torchgeometry/core/
-cd /bodyshop/openpose/build/examples/tutorial_api_python
-rm openpose_python.py
-cp /bodyshop/openpose_python.py .
-cd /bodyshop
+# cp -r /mnt/smplify_x /bodyshop/smplify_x
+# cp /mnt/openpose_python.py /bodyshop/openpose_python.py
+# cp /mnt/conversions.py /usr/local/lib/python3.8/dist-packages/torchgeometry/core/
+# cd /bodyshop/openpose/build/examples/tutorial_api_python
+# rm openpose_python.py
+# cp /bodyshop/openpose_python.py .
+# cd /bodyshop
 
 #Run the code with the relevant params
 xvfb-run -a -s "-screen 0 1024x768x24 -ac +extension GLX +render -noreset" python main.py --config smplify_x/cfg_files/fit_smplx.yaml \
